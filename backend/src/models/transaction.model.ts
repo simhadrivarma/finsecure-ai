543f63215240f509f92ca4223f37d130c555ea93
Simhadri Yadla <simhadrivarmattc@gmail.com>
2026-05-01 16:29:34 +0530

    Add transaction model

---
+++ backend/src/models/transaction.model.ts
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
